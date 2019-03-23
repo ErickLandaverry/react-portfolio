62f79c278048e6c095f3b1c1081e0017c105bb9b
landy <landy.erick20@gmail.com>
2019-03-23 00:12:59 -0600

    implemented styles on the buttons using mixins.

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,5 +1,7 @@
+
 import React, { Component } from "react";
-import axios from 'axios';
+import axios from "axios";
+
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
@@ -17,27 +19,28 @@ export default class PortfolioContainer extends Component {
 
   handleFilter(filter) {
     this.setState({
-      data: this.state.data.filter(item =>{
-        return item.category ===filter;
+      data: this.state.data.filter(item => {
+        return item.category === filter;
       })
-    })
+    });
   }
 
   getPortfolioItems() {
-    axios.get('https://ericklandaverry.devcamp.space/portfolio/portfolio_items')
-    .then(response => {
-      this.setState({
-        data: response.data.portfolio_items
+    axios
+      .get("https://ericklandaverry.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
+        this.setState({
+          data: response.data.portfolio_items
+        });
       })
-    })
-    .catch(error => {
-    console.log(error);
-    });
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   portfolioItems() {
     return this.state.data.map(item => {
-      return <PortfolioItem key={item.id} item ={item} />;
+      return <PortfolioItem key={item.id} item={item} />;
     });
   }
 
@@ -47,19 +50,22 @@ export default class PortfolioContainer extends Component {
 
   render() {
     if (this.state.isLoading) {
-      return <div>Loading...</div>
+      return <div>Loading...</div>;
     }
 
     return (
-      <div>
-        <h2>{this.state.pageTitle}</h2>
+      <div className="portfolio-items-wrapper">
+        <button className="btn" onClick={() => this.handleFilter("HTML")}>
+          HTML
+        </button>
+        <button className="btn" onClick={() => this.handleFilter("JavaScript/React")}>
+          JavaScript/React
+        </button>
+        <button className="btn" onClick={() => this.handleFilter("Python")}>
+          Python
+        </button>
 
-        <button onClick={() => this.handleFilter('html')}>HTML</button>
-        <button onClick={() => this.handleFilter('javescript/react')}>JavaScript/React</button>
-        <button onClick={() => this.handleFilter('ython')}>Python</button>
-        <button onClick={() => this.handleFilter('all')}>All</button>
-
-        <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
+        {this.portfolioItems()}
       </div>
     );
   }
